dba57d92baee8ef52c76d6e0ed42ea40e7dd67c9
Rtollinchi <rtollinchi23@gmail.com>
2024-10-08 12:09:09 -0400

    commit before pull from main 10/8/2024

--- server/routes/funkopop.route.js
+++ server/routes/funkopop.route.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const { FunkoPop } = require("../models");
+const { funkopops } = require("../seedData");
 
 // GET /sauce
 router.get("/", async (req, res, next) => {
   try {
     const funkopops = await FunkoPop.findAll();
     res.send(funkopops);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const funkopop = await FunkoPop.findByPk(req.params.id);
+    res.send(funkopop);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newFunkoPop = await FunkoPop.create(req.body);
+    res.json(newFunkoPop);
   } catch (error) {
     next(error);
   }
