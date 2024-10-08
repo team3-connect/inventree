908178baa2da80689b600e7f64ec20964a2ff200
unknown <rasheem.khan94@gmail.comgit config --global user.name Rasheem>
2024-10-08 12:22:23 -0400

    created a post, update, and delete reqs

--- server/routes/funkopop.route.js
+++ server/routes/funkopop.route.js
@@ -11,12 +11,53 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
 router.get("/:id", async (req, res, next) => {
   try {
-    const funkopops = await FunkoPop.findOne({where: {id:req.params.id}});
+    const funkopops = await FunkoPop.findOne({ where: { id: req.params.id } });
     res.send(funkopops);
   } catch (error) {
     next(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, description, price, category, image } = req.body;
+
+    const newFunko = await FunkoPop.create({
+      name,
+      description,
+      price,
+      category,
+      image,
+    });
+    res.status(201).json(newFunko);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to create item" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { name, description, price, category, image } = req.body;
+
+    const updatedFunko = await FunkoPop.update(
+      { name, description, price, category, image },
+      { where: { id: req.params.id } }
+    );
+    res.json(updatedFunko);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to update item" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await FunkoPop.destroy({ where: { id: req.params.id } });
+    res.status(204);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete item" });
   }
 });
 
