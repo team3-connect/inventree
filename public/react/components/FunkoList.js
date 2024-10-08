dd050e0f4c435e56e43bcd55838c4a87fa81c3c9
ktinson <monikai.tinson@verizon.com>
2024-10-08 15:22:49 -0500

    Added section as container

--- public/react/components/FunkoList.js
+++ public/react/components/FunkoList.js
@@ -5,11 +5,12 @@ export const FunkoList = ({ funkoPops, fetchPage }) => {
   return (
     <>
       {funkoPops.map((funkoPop, idx) => {
-        return( <div><a onClick ={() =>fetchPage(funkoPop.id)}>
-            <Funko funkoPop={funkoPop} key={idx} 
+        return( <div><section className="container"><a onClick ={() =>fetchPage(funkoPop.id)}>
+           <Funko funkoPop={funkoPop} key={idx} 
        /></a>
        <Button variant="outlined"  size="small" className= "pageNav" 
-        style={{borderRadius: "10px", width: "200px"}} onClick ={() =>fetchPage(funkoPop.id)}>{funkoPop.name}</Button></div>);
+        style={{borderRadius: "10px", width: "200px"}} onClick ={() =>fetchPage(funkoPop.id)}>{funkoPop.name}</Button>
+        </section></div>);
 
       })}
     </>
