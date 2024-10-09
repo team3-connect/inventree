3e93e335d40258636f4239ba0d905dd5389f7762
Rvester <rasheem.khan94@gmail.com>
2024-10-09 15:11:47 -0400

    save branch

--- public/react/api.js
+++ public/react/api.js
@@ -1,7 +1,7 @@
 let apiURL;
 
 if (process.env.NODE_ENV === "development") {
-  apiURL = process.env.REACT_APP_API_URL || "/api" ;
+  apiURL = process.env.REACT_APP_API_URL || "/api";
 } else {
   apiURL = "http://localhost:3000/api";
 }
