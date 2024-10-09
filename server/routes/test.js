f6f1b8b8fe1f0bbfd550141cbab0470398e62c45
Rtollinchi <rtollinchi23@gmail.com>
2024-10-09 16:47:18 -0400

    added tests for all routes, all passing

--- server/routes/test.js
+++ server/routes/test.js
@@ -43,4 +43,31 @@ describe("./funkopops routes", () => {
     expect(res.body.name).toBe(newPop.name);
     expect(res.body.category).toBe("Music");
   });
+
+  test("Testing updating a funkopop", async () => {
+    const updatedPop = {
+      name: "CeeDee Lamb",
+      description: "Nfl",
+      category: "Sports",
+      image:
+        "https://funko.com/dw/image/v2/BGTS_PRD/on/demandware.static/-/Sites-funko-master-catalog/default/dwb128eab8/images/funko/upload/81920_POP-NFL_Cowboys_CeeDee-Lamb_POP_GLAM-WEB.png?sw=800&sh=800",
+    };
+
+    const res = await request(app).put("/funkopops/1").send(updatedPop);
+
+    expect(res.statusCode).toBe(200);
+
+    const updatedFunko = res.body.find((funkoPop) => funkoPop.id === 1);
+
+    expect(updatedFunko.name).toBe(updatedPop.name);
+    expect(updatedFunko.description).toBe(updatedPop.description);
+    expect(updatedFunko.category).toBe(updatedPop.category);
+    expect(updatedFunko.image).toBe(updatedPop.image);
+  });
+
+  test("Testing deleting a funkopop", async () => {
+    const res = await request(app).delete("/funkopops/1");
+
+    expect(res.statusCode).toBe(200);
+  });
 });
