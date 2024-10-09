c44ae776b234066dd5b4846e4886e30759a812b4
Rvester <rasheem.khan94@gmail.com>
2024-10-09 17:47:50 -0400

    new

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 const { sequelize } = require("../db");
 
+const { FunkoPop } = require("./FunkoPop");
+
 module.exports = {
   db: sequelize,
   FunkoPop,
